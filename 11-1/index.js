da32c6da45b3fba99e56a6960c3e343a8e671dc7
Ke Chen <CK@QWERT-2.local>
2016-03-07 19:46:13 -0600

    adjust

--- 11-1/index.js
+++ 11-1/index.js
@@ -1,25 +1,34 @@
-
 //Object-oriented Programming
 
 //Person object
-var person = function(){ // This line to the line with "}();" creates a Closure.
+"use strict";
+var person = function () { // This line to the line with "}();" creates a Closure.
     // private data
-    var data = {            // This is an exmaple of a javaScript Object.
-        firstName:'Root first name',
-        $setfirstName: function(n){data.firstName = n },
+
+    var data = { // This is an exmaple of a javaScript Object.
+        firstName: 'Root first name',
+        $setfirstName: function (n) {
+            data.firstName = n;
+        },
 
         lastName: 'Root last name',
-        $setlastName: function(n){data.memo += 1; data.lastName = n},
+        $setlastName: function (n) {
+            data.memo += 1;
+            data.lastName = n;
+        },
 
         email: 'root@email.com',
-        $setEmail: function(n){data.memo += 1; data.email = n}
+        $setEmail: function (n) {
+            data.memo += 1;
+            data.email = n;
+        }
     };
 
-    var F = function(){};
-    f = new F();
+    var F = function () {};
+    var f = new F();
 
     // public data
-    f.sname = 'Substance'
+    f.sname = 'Substance';
 
     //methods
 
@@ -32,25 +41,30 @@ var person = function(){ // This line to the line with "}();" creates a Closure.
 
 //Customer object
 
-var customer = function(p){
+var customer = function (p) {
 
     // private data
     var data = {
-        customerNum:'00000',
-        $setcustomerNum: function(n){data.customerNum = n },
+        customerNum: '00000',
+        $setcustomerNum: function (n) {
+            data.customerNum = n;
+        }
 
     };
 
-    var F = function(){};
-    F.prototype = p;        // The prototype property sets up Inheritance.
-    f = new F();
+    var F = function () {};
+    F.prototype = p; // The prototype property sets up Inheritance.
+    var f = new F();
 
     // public data
-    f.cname = 'customer'
+    f.cname = 'customer';
     f.run = function (e) {
         var r = data[e];
-        if(r === undefined) return F.prototype.run(e);
-        else return r;
+        if (r === undefined) {
+            return F.prototype.run(e);
+        } else {
+            return r;
+        }
     };
 
 
@@ -60,164 +74,167 @@ var customer = function(p){
 
 //Employee Object
 
-var employee = function(p){
+var employee = function (p) {
 
     // private data
     var data = {
-        SSN:'000-00-0000',
-        $setSSN: function(n){data.SSN = n },
+        SSN: '000-00-0000',
+        $setSSN: function (n) {
+            data.SSN = n;
+        }
 
     };
 
-    var F = function(){};
-    F.prototype = p;        // The prototype property sets up Inheritance.
-    f = new F();
+    var F = function () {};
+    F.prototype = p; // The prototype property sets up Inheritance.
+    var f = new F();
 
     // public data
-    f.ename = 'employee'
+    f.ename = 'employee';
     f.run = function (e) {
         var r = data[e];
-        if(r === undefined) return F.prototype.run(e);
-        else return r;
+        if (r === undefined) {
+            return F.prototype.run(e);
+        } else {
+            return r;
+        }
     };
 
     return f;
 }(person);
 
-
+//Function to validate email
+function validateEmail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
 //User interface
 
-$(document).ready(function(){
-    $("#btn1").click(function(){
-        var cont = "y"
+$(document).ready(function () {
+    $("button").click(function () {
+        var cont = "y";
+        $("#btn1").hide();
+        $("#first").hide();
+        $("#btn2").show();
+        $("#second").show();
+        document.getElementById("loading").innerHTML = "";
+        document.getElementById("field").innerHTML = "";
 
-        while (cont == "y") {
-            var person = prompt("Create customer or employee? (c/e):");
 
-            if (person == 'c') {
+
+        while (cont.toLowerCase() === "y") {
+            var person = "";
+            var firstName = "";
+            var lastName = "";
+            var email = "";
+            var social = "";
+            var customerNumber = "";
+            while (person.toLowerCase() !== "c" && person.toLowerCase() !== "e") {
+                person = window.prompt("Create customer or employee? (c/e):");
+            }
+
+            if (person.toLowerCase() === 'c') {
 
                 var c = Object.create(customer);
 
                 document.getElementById("loading").innerHTML =
                     "Creating Customer data...";
-                var firstName = prompt("Enter first name:");
-                c.run('$setfirstName')(firstName)
 
-                var lastName = prompt("Enter Last name:");
-                c.run('$setlastName')(lastName)
+
+                while (firstName === "") {
+                    firstName = window.prompt("Enter first name:");
+                }
+                c.run('$setfirstName')(firstName);
+
+
+                while (lastName === "") {
+                    lastName = window.prompt("Enter Last name:");
+                }
+                c.run('$setlastName')(lastName);
 
 
                 // prompt and validate email
-                var email = false
-                while (validateEmail(email) == false)
-                  var email = prompt("Enter email address")
-                  c.run('$setEmail')(email)
+
+                while (validateEmail(email) === false) {
+                    email = window.prompt("Enter email address");
+                }
+                c.run('$setEmail')(email);
 
 
-                var customerNumber = prompt("Enter Customer number:");
-                c.run('$setcustomerNum')(customerNumber)
+                customerNumber = window.prompt("Enter Customer number:");
+                c.run('$setcustomerNum')(customerNumber);
+                window.alert("You entered: " +
+                    "\nName: " + c.run('firstName') + " " + c.run('lastName') +
+                    "\nEmail: " + c.run('email') +
+                    "\nCustomer number: " + c.run('customerNum'));
 
-
-                $("#field").append("" +
+                $("#field").append(
                     "<div style = 'border:1px solid red;margin:5px;padding:5px;'>" +
-                    "<p>You entered: </p>" +
-                    "<p>Name: " + c.run('firstName') + " " + c.run('lastName') + "</p>" +
-                    "<p>Email: " + c.run('email') + "</p>" +
-                    "<p>Customer number: " + c.run('customerNum') + "</p>" +
-                    "</div>"
+                         "<p>You entered: </p>" +
+                         "<p>Name: " + c.run('firstName') + " " + c.run('lastName') + "</p>" +
+                         "<p>Email: " + c.run('email') + "</p>" +
+                         "<p>Customer number: " + c.run('customerNum') + "</p>" +
+                         "</div>"
                 );
-                var cont = prompt("Continue? (y/n): ");
 
-                document.getElementById("loading").innerHTML =
-                    "Done.";
+
+
             }
 
-            if (person == 'e') {
+            if (person.toLowerCase() === 'e') {
                 var e = Object.create(employee);
 
                 document.getElementById("loading").innerHTML =
                     "Creating Employee data...";
-                var firstName = prompt("Enter first name:");
-                e.run('$setfirstName')(firstName)
 
-                var lastName = prompt("Enter Last name:");
-                e.run('$setlastName')(lastName)
+                while (firstName === "") {
+                    firstName = window.prompt("Enter first name:");
+                }
+                e.run('$setfirstName')(firstName);
+
+
+                while (lastName === "") {
+                    lastName = window.prompt("Enter Last name:");
+                }
+                e.run('$setlastName')(lastName);
 
                 // prompt and validate email
-                var email = false
-                while (validateEmail(email) == false)
-                    var email = prompt("Enter email address")
-                    e.run('$setEmail')(email)
+
+                while (validateEmail(email) === false) {
+                    email = window.prompt("Enter email address");
+                }
+                e.run('$setEmail')(email);
 
 
-                var social = prompt("Social security number:");
-                e.run('$setSSN')(social)
+                while (/^(?!000|666)[0-8][0-9]{2}-(?!00)[0-9]{2}-(?!0000)[0-9]{4}$/.test(social) === false) {
+                    social = window.prompt("Social security number(xxx-xx-xxxx):");
+                }
+                e.run('$setSSN')(social);
 
-                $("#field").append("" +
+                window.alert("You entered: " +
+                    "\nName: " + e.run('firstName') + " " + e.run('lastName') +
+                    "\nEmail: " + e.run('email') +
+                    "\nSocial security number: " + e.run('SSN'));
+
+                $("#field").append(
                     "<div style = 'border:1px solid red;margin:5px;padding:5px;'>" +
-                    "<p>You entered: </p>" +
-                    "<p>Name: " + e.run('firstName') + " " + e.run('lastName') + "</p>" +
-                    "<p>Email: " + e.run('email') + "</p>" +
-                    "<p>Social security number: " + e.run('SSN') + "</p>" +
-                    "</div>"
+                         "<p>You entered: </p>" +
+                         "<p>Name: " + e.run('firstName') + " " + e.run('lastName') + "</p>" +
+                         "<p>Email: " + e.run('email') + "</p>" +
+                         "<p>Social security number: " + e.run('SSN') + "</p>" +
+                         "</div>"
                 );
-                var cont = prompt("Continue? (y/n): ");
 
-                document.getElementById("loading").innerHTML =
-                    "Done.";
+
+
+            }
+            cont = "";
+            while (cont.toLowerCase() !== "y" && cont.toLowerCase() !== "n") {
+                cont = window.prompt("Continue? (y/n): ");
             }
         }
+        document.getElementById("loading").innerHTML =
+            "Done.";
     });
 
 });
-
-//Function to validate email
-function validateEmail(email)
-{
-    var re = /\S+@\S+\.\S+/;
-    return re.test(email);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
